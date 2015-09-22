68da9776e47570b545f39648f96ac07d95e47328
unknown <User@User-PC.ts.local>
2015-09-22 16:42:18 -0300

    criação do projeto web e primeiro servlet

---
+++ src/br/com/caelum/servlet/OiMundo.java
@@ -0,0 +1,28 @@
+package br.com.caelum.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class OiMundo extends HttpServlet{
+	
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+//		super.service(request, response);
+		
+		PrintWriter out = response.getWriter();
+		
+		out.println("<html>");
+		out.println("<body>");
+		out.println("Primeiro Servlet!!!");
+		out.println("</body>");
+		out.println("</html>");
+	}
+	
+
+}
